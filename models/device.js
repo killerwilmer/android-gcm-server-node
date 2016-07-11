a35fcbdf56f954cefd1c6a5bfc72ca30ea81562e
Wilmer Arteaga <ing.wilmerarteaga@gmail.com>
2016-07-11 16:03:39 -0500

    New connection

--- models/device.js
+++ models/device.js
@@ -2,14 +2,15 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var deviceSchema = mongoose.Schema({ 
+var deviceSchema = mongoose.Schema({
 
 	deviceName 		: String,
-	deviceId		: String, 
+	deviceId		: String,
 	registrationId	: String
-	
+
 });
 
-mongoose.connect('mongodb://localhost:27017/node-android-push');
+//mongoose.connect('mongodb://localhost:27017/node-android-push');
+mongoose.connect('mongodb://159.203.223.33:27017/node-android-push');
 
-module.exports = mongoose.model('device', deviceSchema);        
+module.exports = mongoose.model('device', deviceSchema);
